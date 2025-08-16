147c7704743f301797bee212e2188c4e018327f4
talha-zahid1 <mt109208@gmail.com>
2025-08-16 15:01:47 +0500

    Files Added

---
+++ Client.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class Client {
+    public static void main(String args[]) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        Socket socket=new Socket("localhost",9090);
+        PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
+        System.out.println("Enter a Mesaage for server ");
+        String line=sc.nextLine();
+        if (!line.trim().equalsIgnoreCase("Quit")) {
+        while (!line.trim().equalsIgnoreCase("Quit")) {
+            out.println(line);
+            InputStream is=socket.getInputStream();
+            InputStreamReader isr=new InputStreamReader(is);
+            BufferedReader in=new BufferedReader(isr);
+            String respose=in.readLine();
+            System.out.println("Server Response: "+respose);
+            System.out.println("Enter a Mesaage for server ");
+            line=sc.nextLine();    
+        }    
+       }else{
+         out.println("I Don't want to make Communication Sorry!");
+            InputStream is=socket.getInputStream();
+            InputStreamReader isr=new InputStreamReader(is);
+            BufferedReader in=new BufferedReader(isr);
+            String respose=in.readLine();
+            System.out.println("Server Response: "+respose);
+            
+       }
+        out.println(line);
+        InputStream is=socket.getInputStream();
+        InputStreamReader isr=new InputStreamReader(is);
+        BufferedReader in=new BufferedReader(isr);
+        String respose=in.readLine();
+        System.out.println(respose);
+        socket.close();
+    }
+}
---
+++ Server.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    public static void main(String[] args)throws Exception {
+       ServerSocket serverSocket=new ServerSocket(9090);
+       System.out.println("Server is Waiting for  Client ");
+       Socket Clientsocket=serverSocket.accept();
+       System.out.println("Client is Conected ");
+       PrintWriter out=new PrintWriter(Clientsocket.getOutputStream(),true);
+       InputStream is=Clientsocket.getInputStream();
+       InputStreamReader isr=new InputStreamReader(is);
+       BufferedReader in=new BufferedReader(isr);
+       String message=in.readLine();
+       if (!message.trim().equalsIgnoreCase("Quit")) {
+           while (!message.trim().equalsIgnoreCase("Quit")) {
+               System.out.println("Client Says: "+message);
+               out.println("Your Message is Recieved ");
+               message=in.readLine();
+            }
+       }
+        out.println("Communication Ended Successfully");
+        serverSocket.close();
+        Clientsocket.close();
+       
+    }
+}
